0b0da13f0b11922a8238fbe06e1fc5c8962a223a
Anthony <aquinoanthony851@gmail.com>
2024-01-22 08:34:00 -0700

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+  list = []
+  
+  if length == 0:
+    print(list)
+  elif length == 1:
+    list.append(0)
+    print(list)
+  elif length > 1:
+    count = 2
+    list.append(0)
+    list.append(1)
+    while count < length:
+      last_num = list[-1]
+      second_to_last = list[-2]
+      next_number = last_num + second_to_last
+      list.append(next_number)
+      count += 1
+      
+    print(list)
